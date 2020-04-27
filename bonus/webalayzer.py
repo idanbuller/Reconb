0aa3e86e3ec5ddf0ba1fff8d9de10c49149cd98b
idanbuller <49998731+idanbuller@users.noreply.github.com>
2020-04-27 09:27:50 -0700

    Add files via upload

---
+++ bonus/webalayzer.py
@@ -0,0 +1,27 @@
+import webtech
+from prettytable import PrettyTable
+
+class Webalayzer():
+
+    def __init__(self, domain):
+        self.domain = domain
+        self.url = f"https://{domain}"
+
+    def web(self):
+        try:
+            x = PrettyTable()
+            x.field_names = ["Name", "Version", "Module"]
+            wt = webtech.WebTech(options={'json': True, 'random-user-agent': True})
+            report = wt.start_from_url(self.url)
+            for i in range(len(report['tech'])):
+                x.add_row([f"{report['tech'][i]['name']}", f"{report['tech'][i]['version']}", "WebTech"])
+            for i in range(len(report['headers'])):
+                x.add_row([f"{report['headers'][i]['value']}", f"None", "WebTech"])
+            print(x)
+
+        except webtech.utils.ConnectionException:
+          print("Connection error")
+
+#
+# test = Webalayzer("domain.com")
+# test.web()
